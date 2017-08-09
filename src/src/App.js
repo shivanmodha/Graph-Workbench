37d743294ec4f3e3a8e9a723473a335aa189b440
Shivan Modha <shivanmodha@gmail.com>
2017-08-08 23:22:41 -0400

    Navigation.json populates the navigation bar

--- src/src/App.js
+++ src/src/App.js
@@ -1,21 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { ButtonToolbar, ToggleButtonGroup, ToggleButton } from 'react-bootstrap'
+
+const navigation = require('./configuration/navigation.json');
 
 class App extends Component
 {
+    CreateDropdown(element, title)
+    {
+        let links = [];
+        Object.keys(element["children"]).forEach((key) =>
+        {
+            if (element["children"][key] != "divider")
+            {
+                links.push(<MenuItem eventKey={element["children"][key]}>{key}</MenuItem>);
+            }
+            else
+            {
+                links.push(<MenuItem divider />);
+            }
+        });
+        return (
+            <NavDropdown title={title} eventKey={element["eventKey"]} id={element["id"]}>{links}</NavDropdown>
+        );
+    }
+    NavigationCollapse()
+    {
+        let links = {
+            "left": [],
+            "right": []
+        }
+        Object.keys(navigation["links"]).forEach((key) =>
+        {
+            let child = navigation["links"][key];
+            if (child["type"] === "link")
+            {
+                links[child["side"]].push(<NavItem eventKey={child["eventKey"]} href={child["href"]}>{key}</NavItem>);
+            }
+            else if (child["type"] === "dropdown")
+            {
+                links[child["side"]].push(this.CreateDropdown(child, key));
+            }
+        });
+        return (
+            <Navbar.Collapse>
+                <Nav pullLeft> {links["left"]} </Nav>
+                <Nav pullRight> {links["right"]} </Nav>
+            </Navbar.Collapse>
+        );
+    }
     render()
     {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
+            <Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand> {navigation["header"]} </Navbar.Brand>
+                </Navbar.Header>
+                {this.NavigationCollapse()}
+            </Navbar>
         );
     }
 }
